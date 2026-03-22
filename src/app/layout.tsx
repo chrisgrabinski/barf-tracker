7d2ac8fb174d08e98ce6aa8f996ab1a02dd7b780
Chris Grabiński <c.grabinski@gmail.com>
2026-03-22 00:22:21 +0000

    adjust navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { FishIcon, HomeIcon } from "lucide-react";
 import Link from "next/link";
 import { Header } from "@/app/header";
-import { IconButton } from "@/components/icon-button";
+import { Button } from "@/primitives/button";
 
 const geist = Google_Sans({
   subsets: ["latin"],
@@ -23,17 +23,25 @@ export default function RootLayout({
     <html className={geist.className} lang="en">
       <body className="bg-background p-3 text-foreground">
         <Header />
-        <nav className="fixed bottom-8 left-1/2 z-50 flex -translate-x-1/2 gap-2 rounded-full bg-background/25 bg-linear-to-t from-background/33 p-2 backdrop-blur-md backdrop-saturate-150">
-          <IconButton aria-label="Home" asChild>
+        <nav className="fixed bottom-4 left-1/2 z-50 flex w-48 -translate-x-1/2 gap-2 rounded-full bg-background/25 bg-linear-to-t from-background/33 p-2 backdrop-blur-md backdrop-saturate-150">
+          <Button
+            asChild
+            className="grid flex-1 place-items-center gap-0.5 rounded-full bg-neutral-800 p-[0.5ch]"
+          >
             <Link href="/">
-              <HomeIcon />
+              <HomeIcon className="size-6" />
+              <div className="text-xs">Home</div>
             </Link>
-          </IconButton>
-          <IconButton aria-label="Food" asChild>
+          </Button>
+          <Button
+            asChild
+            className="grid flex-1 place-items-center gap-0.5 rounded-full bg-neutral-800 p-[0.5ch]"
+          >
             <Link href="/food">
-              <FishIcon />
+              <FishIcon className="size-6" />
+              <div className="text-xs">Food</div>
             </Link>
-          </IconButton>
+          </Button>
         </nav>
         <main className="container mx-auto grid gap-6">{children}</main>
       </body>
