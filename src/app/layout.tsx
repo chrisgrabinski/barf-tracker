8b2dcf1c50176a925db04bebaa20ef2f12deef7d
Chris Grabiński <c.grabinski@gmail.com>
2026-03-22 00:30:30 +0000

    more adjustments to navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,25 +23,27 @@ export default function RootLayout({
     <html className={geist.className} lang="en">
       <body className="bg-background p-3 text-foreground">
         <Header />
-        <nav className="fixed bottom-4 left-1/2 z-50 flex w-48 -translate-x-1/2 gap-2 rounded-full bg-background/25 bg-linear-to-t from-background/33 p-2 backdrop-blur-md backdrop-saturate-150">
-          <Button
-            asChild
-            className="grid flex-1 place-items-center gap-0.5 rounded-full bg-neutral-800 p-[0.5ch]"
-          >
-            <Link href="/">
-              <HomeIcon className="size-6" />
-              <div className="text-xs">Home</div>
-            </Link>
-          </Button>
-          <Button
-            asChild
-            className="grid flex-1 place-items-center gap-0.5 rounded-full bg-neutral-800 p-[0.5ch]"
-          >
-            <Link href="/food">
-              <FishIcon className="size-6" />
-              <div className="text-xs">Food</div>
-            </Link>
-          </Button>
+        <nav className="pointer-events-none fixed inset-x-0 bottom-0 z-50 flex justify-center bg-linear-to-t from-background px-4 pb-4">
+          <div className="pointer-events-auto flex w-48 gap-1 rounded-full bg-background/25 bg-linear-to-t from-background/33 p-1 backdrop-blur-md backdrop-saturate-150">
+            <Button
+              asChild
+              className="grid flex-1 place-items-center gap-1 rounded-full bg-transparent p-2"
+            >
+              <Link href="/">
+                <HomeIcon className="size-6" />
+                <div className="font-medium text-xs leading-none">Home</div>
+              </Link>
+            </Button>
+            <Button
+              asChild
+              className="grid flex-1 place-items-center gap-1 rounded-full bg-transparent p-2"
+            >
+              <Link href="/food">
+                <FishIcon className="size-6" />
+                <div className="font-medium text-xs leading-none">Food</div>
+              </Link>
+            </Button>
+          </div>
         </nav>
         <main className="container mx-auto grid gap-6">{children}</main>
       </body>
