931347585ca4aa9389a71499125907d7789bbdde
Chris Grabiński <c.grabinski@gmail.com>
2026-03-28 14:49:15 +0000

    add food list

--- src/app/food/page.tsx
+++ src/app/food/page.tsx
@@ -1,16 +1,50 @@
 import { ForkKnifeIcon } from "lucide-react";
+import { cacheLife } from "next/cache";
 import { Card } from "@/components/card";
 
-export default function FoodPage() {
+import { supabase } from "@/lib/supabase";
+
+type FoodProductProps = {
+  name: string;
+  type: string;
+};
+
+const FoodProduct = ({ name, type }: FoodProductProps) => {
   return (
-    <Card className="flex grow flex-col items-center justify-center gap-2 text-center">
-      <div className="grid size-12 place-items-center rounded-lg bg-muted text-muted-foreground">
-        <ForkKnifeIcon />
+    <Card className="flex items-center gap-4">
+      <div className="grid size-16 place-items-center rounded-xl bg-muted text-muted-foreground">
+        <ForkKnifeIcon className="size-6" />
       </div>
-      <p className="font-medium text-xl">Coming soon...</p>
-      <p className="text-balance text-muted-foreground">
-        You will soon be able to track food products right in the app.
-      </p>
+      <div className="flex flex-col gap-1">
+        <span className="font-medium">{name}</span>
+        <span className="text-muted-foreground">{type}</span>
+      </div>
     </Card>
+  );
+};
+
+const getFood = async () => {
+  "use cache";
+
+  cacheLife("hours");
+
+  return supabase.from("food").select("*, type( * )").order("name");
+};
+
+export default async function FoodPage() {
+  const { data: food } = await getFood();
+
+  console.log(food);
+
+  return (
+    <div className="grid gap-4">
+      {food?.map((foodItem) => (
+        <FoodProduct
+          key={foodItem.slug}
+          name={foodItem.name}
+          type={foodItem.type.name}
+        />
+      ))}
+    </div>
   );
 }
